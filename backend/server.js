a3e00d3909e1f465cbce7062a58db374b1e6d906
Videsh Kumar <videshkumar8196@gmail.com>
2026-06-23 17:17:08 +0530

    Refactor server.js for DNS and database connection
    Refactor server.js to improve DNS server configuration and reintroduce database connection.

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
-const app=require('./src/app');
 require('dotenv').config();
-const connectToDB=require('./src/config/database')
-// const invokeGeminiAi=require('./src/services/ai.service');
-port=process.env.PORT
-// const {resume, selfDescription , jobDescription}= require('./src/services/temp');
-// const generateInterviewReport=require('./src/services/ai.service');
+
 const dns=require('dns')
 dns.setServers([
     "8.8.8.8",
     "8.8.4.4"
-])
+]);
+
+const app=require('./src/app');
+const connectToDB=require('./src/config/database');
 connectToDB();
+
+
+// const invokeGeminiAi=require('./src/services/ai.service');
+port=process.env.PORT
+// const {resume, selfDescription , jobDescription}= require('./src/services/temp');
+// const generateInterviewReport=require('./src/services/ai.service');
+
 // invokeGeminiAi();
 // generateInterviewReport({resume, selfDescription, jobDescription});
 
@@ -28,7 +33,6 @@ if (process.env.NODE_ENV !== "production") {
     console.log(
       `Server running on http://localhost:${PORT}`
     );
-
 
   });
 }
